401f05c1695311c8b152b2df207cdf935611dbc5
KyBarrioga <kybarrioga@gmail.com>
2026-02-26 08:53:28 -0700

    chore: fixed linting issues and formats

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,3 +27,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/product/', include('product.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
